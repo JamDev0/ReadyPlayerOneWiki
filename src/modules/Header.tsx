0b7f166bf94c6aa527d9aede8f2682ac3dfe3e27
JAMDev <jamdeveloper0@gmail.com>
2022-06-16 13:27:26 -0300

    sdfdsafdsf

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -1,17 +1,17 @@
 export function Header() {
     return(
         <header
-         className="flex flex-col w-full items-center p-7 gap-y-[20px] text-[#c9d0d3] bg-[#021014] mb-[100px] top-0 left-0"
+         className="flex flex-col w-full items-center p-7 gap-y-[20px] text-[#c9d0d3] bg-[#021014] mb-[100px] top-0 left-0 justify-around"
         >
             <h1
-             className="font-readyPlayerOneFont text-3xl"
+             className="font-readyPlayerOneFont text-3xl text-center"
             >
                 Ready Player One
             </h1>
 
             <nav>
                 <ul
-                 className="flex gap-x-10 text-lg font-bold"
+                 className="flex flex-wrap gap-y-2 gap-x-10 text-lg font-bold justify-around"
                 >
                     <li><a className="hover:underline hover:decoration-[#46d3f0]" href="#info-geral">Info geral</a></li>
                     <li><a className="hover:underline hover:decoration-[#46d3f0]" href="#resumo">Enredo</a></li>
